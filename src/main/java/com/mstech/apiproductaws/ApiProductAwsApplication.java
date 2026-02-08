8cbe5bb89bc4b926f7ab3f86fd4ce3d69fc2aca6
Marcelo Hernandes <marcelohs40@gmail.com>
2026-02-08 12:46:44 -0300

    fix struture of project

--- src/main/java/com/mstech/apiproductaws/ApiProductAwsApplication.java
+++ src/main/java/com/mstech/apiproductaws/ApiProductAwsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiProductAwsApplication {
+public class ApiproductawsApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ApiProductAwsApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ApiproductawsApplication.class, args);
+	}
 
 }
---
+++ src/main/java/com/mstech/apiproductaws/domain/product/Product.java
@@ -0,0 +1,10 @@
+package com.mstech.apiproductaws.domain.product;
+
+import java.lang.annotation.Documented;
+
+@Document(collection = "product")
+public class Product {
+
+
+
+}
