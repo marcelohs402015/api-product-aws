61e96134ba90636b4f97b3bc1cf36a75cdc9a39a
Marcelo Hernandes <marcelohs40@gmail.com>
2026-02-08 08:44:59 -0300

    first commit

---
+++ src/main/java/com/mstech/apiproductaws/ApiProductAwsApplication.java
@@ -0,0 +1,13 @@
+package com.mstech.apiproductaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProductAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiProductAwsApplication.class, args);
+    }
+
+}
